0d15c6708fa2c512e21afaaed789ec289c17f834
jorgeduje <jorgeduje4@gmail.com>
2023-02-17 22:03:03 -0300

    primeros cambios

---
+++ variables.js
@@ -0,0 +1,89 @@
+
+// ANCHOR ---> VARIABLES --- caja que guarda algo
+
+let nombreDeUsuario = 'Jorge' // o "Jorge"
+
+/*
+
+sada
+dsadas
+asdadsa
+dsadsa
+dsad
+*/
+
+const id = "12312dasdas1231"
+
+console.log( nombreDeUsuario )
+
+nombreDeUsuario = "pepito"
+nombreDeUsuario = "juancito"
+
+console.log( nombreDeUsuario )
+
+// console.log(apellido)
+
+// apellido = "Gonzalez" // NO SE PUEDE POR QUE ESTA CON CONST
+
+// LINK --- TIPOS DE DATOS
+
+// ANCHOR ---> strings = cadena de caracteres o cadena de texto
+
+let frase = "Hola como estas?"
+
+// ANCHOR ---> number = numeros
+
+let edad = 30 // enteros
+let precio = 10.5 // decimales
+
+console.log( precio )
+
+
+// ANCHOR ---> booleanos = boolean
+
+let estaLogeado = true // false
+
+let estaCasado = false
+let tieneMascotas = true
+
+// ANCHOR ---> undefined
+
+let email = undefined
+
+console.log(email)
+
+// null
+// NaN
+
+// OPERADORES
+
+let nombre = "juancito"
+let apellido = "lopez"
+
+let nombreCompleto = nombre + " " + apellido
+let saludo = "Hola" + " " + nombre + " " + "como estas?"
+let saludo2 = `Hola ${nombre} como estas?`
+
+console.log( saludo )
+console.log( saludo2 )
+
+let num1 = 10
+let num2 = 3
+
+let suma = num1 + num2
+let multi = num1 * num2
+let division = num1 / num2
+let resta = num1 - num2
+let modulo = num1 % num2 // ---> Me devuelve el resto
+
+console.log(suma)
+console.log(multi)
+console.log(division)
+console.log(resta)
+console.log(modulo)
+
+// OPERADORES DE COMPARACION
+
+// OPERADORES LOGICOS 
+
+
